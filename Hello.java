cfe9af27f2630dbe737f2a0b1e03e19f68edf0b2
manosahoo <57607347+manosahoo@users.noreply.github.com>
2020-09-09 23:39:50 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-class Simple{  
+Public class Hello{  
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
      System.out.println("Periodic Java Class Call in every 5 minutes"); 
